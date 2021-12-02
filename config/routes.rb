ffca796c899e21d0ceca9c91eb66ecd51b1cfb24
Karen Olson <karmarie@gmail.com>
2021-12-01 21:01:57 -0600

    Create nested route for meetings between a particular teacher and student.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :meetings
   resources :students, only: [:index]
-  resources :teachers, only: [:index, :show]
+  resources :teachers, only: [:index, :show] do 
+    resources :students do 
+      resources :meetings, only: [:index]
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
