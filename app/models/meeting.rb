f14d750ec86dbc68e314f70a8cb381d67381b0bd
Karen Olson <karmarie@gmail.com>
2021-12-04 10:58:01 -0600

    Added custom method to Meeting model, created serializer to return formatted meeting date with meeting.

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -7,4 +7,8 @@ class Meeting < ApplicationRecord
     def self.teacher_student_meetings(teacher_id, student_id)
         meetings = self.where(["teacher_id = ? and student_id = ?", teacher_id, student_id]).order('date DESC')
     end
+
+    def formatted_date
+        self.date.strftime("%b %d, %Y")
+    end
 end
---
+++ app/serializers/meeting_serializer.rb
@@ -0,0 +1,3 @@
+class MeetingSerializer < ActiveModel::Serializer
+  attributes :id, :teacher_id, :student_id, :duration, :notes, :date, :formatted_date
+end
