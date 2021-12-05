f619d163317dc929d3b8121c500283e7a61671c0
Karen Olson <karmarie@gmail.com>
2021-12-05 09:11:04 -0600

    Debug CORS issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://special-education-attendance-tracker.netlify.app/* '
+    origins 'https://special-education-attendance-tracker.netlify.app'
 
     resource '*',
       headers: :any,
